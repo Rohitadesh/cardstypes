4d7df30b22018a5d4056f5fa95bd56a2022fbd07
Rohitadesh <adeshkumarnagalikar2000@gmail.com>
2024-08-29 15:08:27 +0530

    data commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
 const Navbar = () =>{
 
-
+    
 
     return(
-        <div className="h-[12rem] w-[23rem] border border-black">
-
+        <div className="h-[28rem] w-[48rem] gap-12 rounded-md  flex flex-row justify-center items-center">
+            <div className="h-[18rem] rounded-md w-36  pl-4 ">
+                <div className=" h-[6rem] w-[6rem] border shadow-md rounded-full  flex flex-col justify-center items-center">
+                    <div className="h-[5rem] w-[5rem] flex flex-col justify-center items-center bg-sky-800 drop-shadow-2xl  rounded-full  ">
+                            <h1 className="text-md text-white text-center">HTML</h1>
+                    </div>
+                </div>
+                <div className="h-[6rem] w-[6rem]  gap-[2px]  flex flex-col justify-start items-center">
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-1.5 w-1.5 bg-black border rounded-full"></div>
+                    <div className="h-6 w-6 bg-yellow-200 border rounded-full"></div>
+                 
+                  
+                </div>
+            </div>
+            
         </div>
     )
 }
